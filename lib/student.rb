a7a7a6451d51ad7b9cb4960ac68f920abbf61919
david <davidjhoyjr@gmail.com>
2022-07-01 13:31:21 -0400

    commit tree 35bfa226d3de3ced09c5f715ddd446f9ab63609f

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+require_relative './user'
+class Student < User
+    attr_accessor :knowledge
+
+    def initialize
+        @knowledge = []
+    end 
+
+    def learn (know)
+        self.knowledge << know
+
+    end
+
+    # def knowledge
+    #     self.knowledge
+    # end
 
 end
